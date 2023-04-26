7cccf5f70ac05ea7eb42a240167557ad63a2920c
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-04-27 02:33:03 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,30 @@
-# 미로문제
-# 상하좌우가 미로(범위)를 벗어나지 않고,
-# 1인 값으로 이동하면서 값을 +1 씩 해줄 것..
-# 사실상 미로에서 이동할 수 있는 모든 위치들에 대해 이동값을 구하고 목적지의 값을 구하는 것
-
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-matrix = []
-for _ in range(n):
-    matrix.append(list(map(int, input().rstrip())))
+N, M = map(int, input().split())
+MIRO = []
+for i in range(N):
+    MIRO.append(list(map(int, input().rstrip())))
 
+# 상 하 좌 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
 def bfs(x, y):
-    que = deque([(x, y)])
-    while que:  # que 가 비어있지 않다면 반복..
-        x, y = que.popleft()  # que에 튜플로 담긴 값을 각각 뽑아내는 방법이군아..
-
+    Q = deque([(x, y)])
+    while Q:
+        x, y = Q.popleft()
         for i in range(4):
-            next_x = x + dx[i]
-            next_y = y + dy[i]
-            if (
-                0 <= next_x < n and 0 <= next_y < m and matrix[next_x][next_y] == 1
-            ):  # x,y좌표가 미로 밖을 벗어나지 않고 해당 칸 값이 1인 경우
-                que.append((next_x, next_y))
-                matrix[next_x][next_y] = matrix[x][y] + 1
+            nx = x + dx[i]  # 다음 좌표가 될 수 있는 4가지 경우
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and MIRO[nx][ny] == 1:
+                Q.append([nx, ny])
+                MIRO[nx][ny] = MIRO[x][y] + 1
+                if nx == (N - 1) and ny == (M - 1):
+                    return MIRO[nx][ny]
 
 
-bfs(0, 0)
-print(matrix[n - 1][m - 1])
+print(bfs(0, 0))
