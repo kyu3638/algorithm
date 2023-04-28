1a5794649f9549203629724be5aa72767496655c
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-04-29 00:03:03 +0900

    [Gold V] Title: 동전, Time: 160 ms, Memory: 35656 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    coins.insert(0,0)
+    money = int(input())
+    
+    dp = [[0 for _ in range(money+1)] for _ in range(N+1)]
+    for i in range(N+1):
+        dp[i][0] =1
+        
+    for i in range(1,N+1):
+        for j in range(1, money+1):
+            dp[i][j] = dp[i-1][j]
+            if j - coins[i] >=0:
+                dp[i][j] = dp[i][j] + dp[i][j-coins[i]]
+                
+    print(dp[N][money])
