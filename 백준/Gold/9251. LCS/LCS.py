cedc07f47503c15cdc9e6be5f2367f6d1823c9fe
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-05-03 05:57:22 +0900

    [Gold V] Title: LCS, Time: 508 ms, Memory: 56572 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,21 +1,22 @@
 import sys
+
 input = sys.stdin.readline
 
-N_list=list(input().rstrip())
-N_list.insert(0,0)
-N = len(N_list)
+a = list(input().rstrip())
+a.insert(0, 0)
+N = len(a)
 
-M_list = list(input().rstrip())
-M_list.insert(0,0)
-M = len(M_list)
+b = list(input().rstrip())
+b.insert(0, 0)
+M = len(b)
 
 dp = [[0 for _ in range(M)] for _ in range(N)]
 
-for i in range(1,N):
-    for j in range(1,M):
-        if N_list[i] == M_list[j]:
-            dp[i][j] = dp[i-1][j-1]+1
-        elif N_list[i] != M_list[j]:
-            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-            
-print(dp[N-1][M-1])
+for i in range(1, N):
+    for j in range(1, M):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[N - 1][M - 1])
