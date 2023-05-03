5003757ae1383854646257d1936f1898e16ce4d5
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-05-03 21:59:59 +0900

    [Silver I] Title: 신입 사원, Time: 4784 ms, Memory: 47156 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,25 +1,26 @@
+# 지원자 A 의 성적이 다른 지원자 B의 성적에 비해 서류 심사 결과와 면접 성적 모두 떨어진다면 A는 결코 선발되지 않는다.
+# 1차 시험의 성적을 오름차순으로 정렬
+# 2차 시험의 성적이 앞선 사람보다 낮으면 탈락(둘다 낮기 때문에!)
+
 import sys
 
 input = sys.stdin.readline
 
 T = int(input())
-answer = []
+
 for _ in range(T):
     N = int(input())
-    grade = []
+    rank = []
     for _ in range(N):
         a, b = map(int, input().split())
-        grade.append([a, b])
-    grade.sort()  # 서류 성적 오름차순
+        rank.append([a, b])
+    rank.sort()
 
     count = 1
-    condition = grade[0][1]
-    for i in grade[1:]:
+    condition = rank[0][1]
+    for i in rank[1:]:
         if condition > i[1]:
-            condition = i[1]
             count += 1
+            condition = i[1]
 
-    answer.append(count)
-
-for i in range(T):
-    print(answer[i])
+    print(count)
