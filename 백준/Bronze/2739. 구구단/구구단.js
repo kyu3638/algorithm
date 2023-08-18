d52b3f90e5fbe3be8d42d11dd16051258da31458
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-08-19 01:33:27 +0900

    [Bronze V] Title: 구구단, Time: 152 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,8 @@
+// let input = require('fs').readFileSync('2739.txt').toString();
+let input = require('fs').readFileSync('dev/stdin').toString();
+
+input = Number(input);
+
+for (let i = 1; i < 10; i++) {
+  console.log(`${input} * ${i} = ${input * i}`);
+}
