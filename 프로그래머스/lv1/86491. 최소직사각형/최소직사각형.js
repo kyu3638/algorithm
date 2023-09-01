22f1751c104bac5ea3eb293f92a81c44d883c74d
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-09-02 02:30:20 +0900

    [level 1] Title: 최소직사각형, Time: 3.53 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,19 +1,18 @@
 function solution(sizes) {
-    // var answer = 0;
-    // return answer;
+    // 가로와 세로의 길이를 담을 배열
     let width = [];
     let height = [];
     
+    // 배열에 가로와 세로의 길이를 담아주고
     for(let i = 0; i < sizes.length; i++){
         width.push(sizes[i][0]);
         height.push(sizes[i][1]);
     }
-    // console.log(width);
-    // console.log(height);
+    // 가로, 세로의 최대값을 구한다.
     let maxWidth = Math.max(...width);
     let maxHeight = Math.max(...height);
-    // console.log(maxWidth, maxHeight)
     
+    // 세로의 길이가 더 긴 index의 가로와 세로를 뒤바꿔준다.
     for(let i = 0; i<sizes.length; i++){
         if(width[i] < height[i]){
             const a = width[i];
@@ -22,7 +21,7 @@ function solution(sizes) {
             height[i] = a;
         }
     }
-    // console.log(width);
-    // console.log(height);
+    
+    // 변경된 가로와 세로의 길이의 최대값의 곱이 최소직사각형의 크기를 가지는 지갑
     return Math.max(...width) * Math.max(...height);
 }
