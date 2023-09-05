b36e390dc4812d1fa00daab5c745e7b60418ebfc
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-09-05 14:39:51 +0900

    [level 2] Title: 타겟 넘버, Time: 1.08 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,13 +1,13 @@
 function solution(numbers, target) {
-    let answer = 0;
-    const dfs = (length, sum) => {
+    let count = 0;
+    const dfs = (length,sum) => {
         if(length === numbers.length){
-            if(sum === target) answer++;
+            if(sum === target) count++;
         } else {
-            dfs(length + 1, sum + numbers[length]);
-            dfs(length + 1, sum - numbers[length]);
+            dfs(length+1,sum + numbers[length]);
+            dfs(length+1,sum - numbers[length]);
         }
     }
     dfs(0,0);
-    return answer;
+    return count;
 }
