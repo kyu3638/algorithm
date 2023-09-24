33abf69cc5d2b8968c559c3176a588eb04518abc
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-09-24 23:37:43 +0900

    [level 1] Title: 모의고사, Time: 3.21 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -2,7 +2,8 @@ function solution(answers) {
     var answer = [];
     // 각 수포자의 점수를 담을 배열 초기화
     let score = [0, 0, 0];
-    // 5, 8, 10
+    
+    // 각 수포자들의 찍기 패턴
     const answerFirst = [1, 2, 3, 4, 5];
     const answerSecond = [2, 1, 2, 3, 2, 4, 2, 5];
     const answerThird = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
@@ -23,10 +24,9 @@ function solution(answers) {
     }
     
     const maxNum = Math.max(...score);
-    // console.log(maxNum);
-    if(maxNum === score[0]) answer.push(1);
-    if(maxNum === score[1]) answer.push(2);
-    if(maxNum === score[2]) answer.push(3);
+    for(let i = 0; i < score.length; i++){
+        if(score[i] === maxNum) answer.push(i+1);
+    }
     
     return answer;
 }
