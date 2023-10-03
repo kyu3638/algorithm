d97a9523ca9df90accb912363319312bfe6e1313
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-10-04 03:41:34 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    var answer = [];
+
+    
+    for(let i = 0; i < my_string.length; i++){
+        if(Number.isInteger(Number(my_string[i]))){
+            answer.push(Number(my_string[i]));
+        }
+    }
+    return answer.sort((a,b)=>a-b);
+}
