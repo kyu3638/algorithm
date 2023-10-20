cfac74c749c825d3b4d14778dad55b00e9264166
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-10-21 01:22:18 +0900

    [level 2] Title: 구명보트, Time: 10.31 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,15 +1,4 @@
 function solution(people, limit) {
-    let answer = 0;
-    people.sort((a,b)=>a-b);
-    let pl = 0;
-    let pr = people.length - 1;
-    
-    while(pl <= pr){
-        if(people[pl] + people[pr] <= limit){
-            pl++;
-        }
-        pr--;
-        answer++;
-    }
+    var answer = 0;
     return answer;
 }
