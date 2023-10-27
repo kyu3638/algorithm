2295967721235e918b1911eb2e114d14af26e58f
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-10-27 23:38:25 +0900

    [level unrated] Title: 팩토리얼, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    
+    function factorial(num){
+        if(num <= 1) return 1;
+        return num * factorial(num - 1);
+    }
+
+
+    for(let i = 1; i <= n; i++){
+        if(factorial(i) > n) return i - 1;
+        if(factorial(i) === n) return i;
+    }
+}
