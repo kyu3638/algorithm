ff21f9fee0567fac7d457e9aaaaed2dd2a932a17
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2023-12-21 13:37:25 +0900

    [level 2] Title: 디펜스 게임, Time: 0.65 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
@@ -0,0 +1,109 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  pop() {
+    if (this.isEmpty()) return null;
+
+    const root = this.heap[0];
+    const lastNode = this.heap.pop();
+
+    if (!this.isEmpty()) {
+      this.heap[0] = lastNode;
+      this.heapifyDown();
+    }
+
+    return root;
+  }
+    
+size(){
+    return this.heap.length;
+}
+    
+    getMin(){
+        return this.heap[0];
+    }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[parentIndex] <= this.heap[index]) break;
+      [this.heap[parentIndex], this.heap[index]] = [
+        this.heap[index],
+        this.heap[parentIndex],
+      ];
+      index = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const length = this.heap.length;
+
+    while (true) {
+      let smallest = index;
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+
+      if (
+        leftChildIndex < length &&
+        this.heap[leftChildIndex] < this.heap[smallest]
+      ) {
+        smallest = leftChildIndex;
+      }
+
+      if (
+        rightChildIndex < length &&
+        this.heap[rightChildIndex] < this.heap[smallest]
+      ) {
+        smallest = rightChildIndex;
+      }
+
+      if (smallest === index) break;
+
+      [this.heap[index], this.heap[smallest]] = [
+        this.heap[smallest],
+        this.heap[index],
+      ];
+      index = smallest;
+    }
+  }
+}
+
+
+function solution(n, k, enemy) {
+    const heap = new MinHeap();
+    let answer = 0;
+    
+    for(let i = 0; i < enemy.length; i++){
+        if(heap.size() < k){
+            heap.push(enemy[i]);
+            answer++;
+        } else {
+            let min = heap.getMin();
+            
+            if(enemy[i] > min){
+                n-=min;
+                heap.pop();
+                heap.push(enemy[i]);
+            }
+            else {
+                n -= enemy[i];
+            }
+            
+            if(n >= 0) answer++;
+        }
+    }
+    return answer;
+}
