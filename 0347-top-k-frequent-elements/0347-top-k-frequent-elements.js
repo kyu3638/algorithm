b5ced1508680b8b54d77271bad5171d01f7a55dd
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2024-01-05 15:13:17 +0900

    Time: 68 ms (58.22%), Space: 45.1 MB (63.76%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const numObj = {};
+    for(let i = 0; i < nums.length; i++){
+        if(!numObj[nums[i]]) numObj[nums[i]] = [nums[i], 1];
+        else numObj[nums[i]][1] += 1;
+    }
+
+    const numArr = Object.values(numObj)
+    numArr.sort((a,b)=>b[1]-a[1])
+    const answer = [];
+
+    for(let i = 0; i < k; i++){
+        answer.push(numArr[i][0])
+    }
+
+    return answer;
+};
