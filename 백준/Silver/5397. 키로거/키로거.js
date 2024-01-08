41e2ebad89bbb856b606a5a863d39451451912c8
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2024-01-08 16:25:59 +0900

    [Silver II] Title: 키로거, Time: 676 ms, Memory: 189428 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.js
+++ 백준/Silver/5397. 키로거/키로거.js
@@ -1,29 +1,24 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().split('\n');
 
 const T = input.shift();
-for (let i = 0; i<T; i++) {
-    password(input[i].split(''))
+for (let i = 0; i < T; i++) {
+  password(input[i].split(''));
 }
 
 function password(str) {
-    let front = [];
-    let back = [];
-    str.forEach((kp) => {
-        if (kp == '-') {
-            front.length == 0 || front.pop();
-        }
-        else if (kp == '>') {
-            back.length == 0 || front.push(back.pop());
-        }
-        else if (kp == '<') {
-            front.length == 0 || back.push(front.pop());
-        }
-        else {
-            front.push(kp);
-        }
-        // console.log('front: ',front);
-        // console.log('back: ',back);
-    })
-    console.log(front.join('')+back.reverse().join(''));
+  let front = [];
+  let back = [];
+  str.forEach(kp => {
+    if (kp == '-') {
+      front.length == 0 || front.pop();
+    } else if (kp == '>') {
+      back.length == 0 || front.push(back.pop());
+    } else if (kp == '<') {
+      front.length == 0 || back.push(front.pop());
+    } else {
+      front.push(kp);
+    }
+  });
+  console.log(front.join('') + back.reverse().join(''));
 }
