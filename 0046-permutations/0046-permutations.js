19f0a36af667e23f9cc7bf525e11a18e10d7b98e
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2024-01-10 16:35:28 +0900

    Time: 68 ms (38.21%), Space: 45 MB (46.03%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -5,16 +5,11 @@
 var permute = function(nums) {
     const answer = [];
     function backtrack(nums,path){
-
         if(nums.length === 0){
             answer.push(path);
         }
 
         for(let i = 0; i < nums.length; i++){
-            console.log(`nums : `, nums);
-            console.log(`path : `, path);
-            console.log(`[...nums.slice(0,${i}),...nums.slice(${i+1})] : `, [...nums.slice(0,i),...nums.slice(i+1)])
-            console.log(`[...path,nums[${i}]] : `,[...path,nums[i]])
             backtrack([...nums.slice(0,i),...nums.slice(i+1)], [...path,nums[i]])
         }
     }
