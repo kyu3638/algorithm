75d15417e3d10cf113762564afac8a5f2b2b1304
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2024-01-18 12:40:09 +0900

    [Gold V] Title: 센서, Time: 192 ms, Memory: 11124 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.js
@@ -0,0 +1,19 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+// 고속도로에 n개의 센서
+// k개의 집중국
+const N = +input[0];
+const K = +input[1];
+
+const arr = input[2]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const minus = [];
+for (let i = 0; i < arr.length - 1; i++) {
+  minus.push(arr[i + 1] - arr[i]);
+}
+minus.sort((a, b) => b - a);
+
+console.log(minus.slice(K - 1, N - 1).reduce((acc, cur) => acc + cur, 0));
