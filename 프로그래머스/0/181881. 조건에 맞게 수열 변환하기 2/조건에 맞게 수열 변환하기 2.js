3c79f26132226fe83678d0648f4259a5242525d4
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2024-01-19 18:56:02 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 4.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,34 @@
+function solution(arr) {
+    
+    let prev = [...arr]
+    let current = [...arr]
+    let count = 0;
+    let flag = false;
+    while(flag === false){
+        current = current.map(num=>{
+            if(num >= 50 && num % 2 === 0){
+                return num / 2;
+            } else if (num < 50 && num % 2 === 1){
+                return 2 * num + 1;
+            } else {
+                return num
+            }
+        })
+        console.log(prev)
+        console.log(current)
+        for(let i = 0; i < current.length; i++){
+            if(prev[i] === current[i]) {
+                flag = true;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) return count;
+        count++;
+        prev = [...current]
+        // flag = true;
+    }
+    
+    return count;
+}
