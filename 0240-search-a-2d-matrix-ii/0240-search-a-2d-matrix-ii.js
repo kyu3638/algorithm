66626ac4ef9b403edba7ea14da08bca2d9b46b8f
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2024-01-19 15:58:51 +0900

    Time: 272 ms (86.19%), Space: 44.6 MB (91.21%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let i = 0;
+    let j = matrix[0].length - 1;
+
+
+    while(i < matrix.length && j >= 0){
+        if(matrix[i][j] === target){
+            return true;
+        } else if(matrix[i][j] > target){
+            j--;
+        } else if(matrix[i][j] < target){
+            i++;
+        }
+    }
+    return false;
+};
