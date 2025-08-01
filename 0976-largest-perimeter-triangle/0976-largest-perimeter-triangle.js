acb374a448964800c33ac23f79c83fdb64af4a8d
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2025-08-02 03:58:49 +0900

    Time: 25 ms (76.8%), Space: 57.6 MB (53.6%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    nums.sort((a,b) => b-a);
+    
+    for(let i = 0; i <= nums.length - 3; i++) {
+        if(nums[i+1] + nums[i+2] > nums[i]) {
+            return nums[i] + nums[i+1] + nums[i+2];
+        }
+    }
+    return 0;
+};
