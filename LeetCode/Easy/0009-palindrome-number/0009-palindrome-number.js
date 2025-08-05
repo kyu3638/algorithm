1ae60f684e5b4e1b7bbf2f605d0a97f2f6dd83dd
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2025-08-06 00:46:10 +0900

    Time: 6 ms (67.14%), Space: 65.1 MB (25.29%) - LeetHub

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x < 0) return false;
+
+    const arr = String(x).split("");
+    
+    for(let i = 0; i < Math.floor(arr.length / 2); i++) {
+        if(arr[i] !== arr[arr.length - 1 - i]) {
+            return false;
+        }
+    }
+    
+    return true;
+};
