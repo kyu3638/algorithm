b9806f4bd1dccbd626924f965a26da207b469c37
kyu3638 <121871179+kyu3638@users.noreply.github.com>
2025-08-08 01:26:32 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.46 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,32 +1,50 @@
 function solution(n, wires) {
-    const answer = [];
-    for(let k = 0; k <= wires.length; k++){
-        const map = [...Array(n+1)].map(()=>[]);
-        const visited = [...Array(n+1)].map(()=>false);
-        function bfs(start){
-            const q = [start];
-            visited[start] = true;
-            let count = 1;
-            while(q.length){
-                const num = q.shift();
-                for(let i = 0; i < map[num].length; i++){
-                    if(!visited[map[num][i]]){
-                        q.push(map[num][i]);
-                        visited[map[num][i]] = true;
-                        count++;
-                    }
+    // 송전탑(노드)이 전선(간선)을 통해 트리 형태로 연결되어 있음
+    // 간선을 하나 끊어서 전력망 네트워크를 2개로 분할하고자 함
+    // 두 전력망이 비슷한 수의 송전탑을 갖도록 하는 것이 목표
+    
+    // 결국 wire하나를 없애서 만들어지는 두 트리의 송전탑 개수의 차이를 반환하면 됨
+    
+    
+    const diffs = [];
+    const bfs = (obj) => {
+        const visited = Array.from({length:n+1}, () => false);
+        
+        const queue = [1];
+        visited[1] = true;
+        let front = 0;
+        let count = 1;
+        
+        while(front < queue.length) {
+            const node = queue[front++];
+            const connected = obj[node] || [];
+            for(const n of connected) {
+                if(visited[n] === false) {
+                    visited[n] = true;
+                    queue.push(n);
+                    count++;
                 }
             }
-            return count;
         }
-        wires.forEach(([a,b],i)=>{
-            if(i === k) return;
-            map[a].push(b);
-            map[b].push(a);
-        })
-        const a = bfs(1);
-        const b = n - a;
-        answer.push(Math.abs(a-b))
+        
+        return count;
     }
-    return Math.min(...answer)
+    
+    for (let i = 0; i < wires.length; i++) {
+        // i 번째 인덱스 없이 간다.
+        const removed = wires.filter((_,index) => index !== i);
+        const obj = {};
+        removed.forEach(([from, to]) => {
+            if(!obj[from]) obj[from] = [];
+            if(!obj[to]) obj[to] = [];
+            obj[from].push(to);
+            obj[to].push(from);
+        })
+        
+        const count = bfs(obj);
+        const otherCount = n - count;
+        diffs.push(count > otherCount ? count - otherCount : otherCount - count);
+    }
+    
+    return Math.min(...diffs);
 }
